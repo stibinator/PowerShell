ffe56ac6d8e9ef11acf9495f399d2bdc88889024
stib <stib@pureandapplied.com.au>
2021-07-22 12:45:31 +1000

    fixed bugs with folder paths in new-aescript

--- Modules/New-AEScript/New-AEScript.psm1
+++ Modules/New-AEScript/New-AEScript.psm1
@@ -1,18 +1,18 @@
 function New-AEScript {
     param (
-    [string]$scriptName = $(
-    $name = $(read-host "ScriptName pls ");
-    if (-not $name) {$(throw "no name supplied")}
-    $name),
-    [string]$developmentDir,
-    [switch]$UIScript,
-    [string]$template = $(if ($UIScript){"UI-Script"} else {"selectedLayersTemplate"}),
-    [switch]$overWriteOK,
-    [switch]$createHardlinkInAEScriptsFolder
+        [string]$scriptName = $(
+            $name = $(read-host "ScriptName pls ");
+            if (-not $name) { $(throw "no name supplied") }
+            $name),
+        [string]$developmentDir,
+        [switch]$UIScript,
+        [string]$template = $(if ($UIScript) { "UI-Script" } else { "selectedLayersTemplate" }),
+        [switch]$overWriteOK,
+        [switch]$createHardlinkInAEScriptsFolder
     )
     
     $prefs = getPrefs; 
-    if ("" -ne $developmentDir){
+    if ("" -ne $developmentDir) {
         $prefs.developmentDir = $developmentDir
     } 
     if (! (test-path $prefs.developmentDir -ErrorAction SilentlyContinue)) {       
@@ -20,58 +20,65 @@ function New-AEScript {
     }
         
     $hardLinkTargetFolder = Join-Path $env:APPDATA "Adobe" "After Effects"
-    $aeversions = Get-ChildItem $hardLinkTargetFolder -dir|Where-Object{$_.name -match (".*[0-9]+")}
+    $aeversions = Get-ChildItem $hardLinkTargetFolder -dir | Where-Object { $_.name -match (".*[0-9]+") }
     
-    $latestVersion = 0;
-    foreach($n in $aeversions){
-        if ([float]$latestVersion -le [float]$n.name){ $latestVersion = $n.name}
+    $latestVersionNum = 0;
+    foreach ($n in $aeversions) {
+        if ($n.name -match "([0-9]+.)([0-9]*)\.*([0-9]*)$") {
+            $version = [float]($Matches[1] + $Matches[2])
+            if ([float]$latestVersionNum -le $version) { 
+                $latestVersionNum = $version
+                $latestVersion = $n.name
+            }
+        }
     }
     $hardLinkTargetFolder = Join-Path $hardLinkTargetFolder $latestVersion "Scripts"
     
     # append "scriptUI panels" to the path
-    if ($UIScript){ 
+    if ($UIScript) { 
         $outputDir = Join-Path $prefs.developmentDir "ScriptUI Panels"
         $hardLinkTargetFolder = Join-Path $hardLinkTargetFolder "ScriptUI Panels"
-    } else {
+    }
+    else {
         $outputDir = $prefs.developmentDir
     }
     
     # give it the extension
-    if (-not($scriptName -match "(.*).jsx$")){
+    if (-not($scriptName -match "(.*).jsx$")) {
         $scriptName = $scriptName + ".jsx";
     }
     # compile the file and hardlink names
     $newScriptPath = Join-Path $outputDir $scriptName
-    if ($createHardlinkInAEScriptsFolder){
+    if ($createHardlinkInAEScriptsFolder) {
         $hardLinkPath = (Join-Path $hardLinkTargetFolder $scriptName)
     }
     
     #get the template
     $templatePath = Join-Path $PSScriptRoot  "$template.txt"; #templates are in the same dir as the PS script
-    if (-not(Test-Path $templatePath)){
+    if (-not(Test-Path $templatePath)) {
         throw "Template does not exist" #Silly rabbit
     }
     
     $templateText = Get-Content $templatePath
     $scriptText = @()
-    $templateText|ForEach-Object{
+    $templateText | ForEach-Object {
         $scriptText += $_.replace("#scriptname#", $scriptName.replace(".jsx", ""))
     }
     # check for pre-existing files
     $okToGo = $true
-    if ((Test-Path $newScriptPath) -and (-not $overWriteOK)){
+    if ((Test-Path $newScriptPath) -and (-not $overWriteOK)) {
         $okToGo = $(
-        Write-Host "$scriptName already exists in $outputDir" -ForegroundColor DarkYellow;
-        write-host "Overwite? " -NoNewline -ForegroundColor Yellow;
-        read-host "(y/N)"
+            Write-Host "$scriptName already exists in $outputDir" -ForegroundColor DarkYellow;
+            write-host "Overwite? " -NoNewline -ForegroundColor Yellow;
+            read-host "(y/N)"
         ).ToLower() -match "^y$"
     }
     # do the things
-    if ($okToGo){
+    if ($okToGo) {
         try {
             Set-Content -Path $newScriptPath -Value $scriptText
             Write-Host "Created $scriptName in $outputDir" -ForegroundColor Green
-            if ($createHardlinkInAEScriptsFolder){
+            if ($createHardlinkInAEScriptsFolder) {
                 cmd /c mklink /h "$newScriptPath"  "$hardLinkPath" 
                 New-Hardlink $hardLinkPath $newScriptPath
                 Write-host "Hardlinked script to AE Scripts folder $hardLinkTargetFolder" -ForegroundColor Green
@@ -81,21 +88,31 @@ function New-AEScript {
             Write-Host "An error occured" -ForegroundColor Red
             Write-Host $_ -ForegroundColor DarkRed
         }
-    } else {
+    }
+    else {
         Write-host "Script wasn't written." -ForegroundColor DarkYellow
     }
+    savePrefs($prefs)
+    
+}
+function savePrefs($prefs){
+    mkdir (join-path $env:APPDATA "pureandapplied") -ErrorAction SilentlyContinue  2>&1 >$null
     Export-Clixml -Path  (join-path $env:APPDATA "pureandapplied" "newAeScript.dat") -InputObject $prefs
 }
 
-function getPrefs(){
+function getPrefs() {
     $prefs = @{}
-    $prefs = import-clixml (join-path $env:APPDATA "pureandapplied" "newAeScript.dat") -ErrorAction SilentlyContinue;
+    $prefFile = join-path $env:APPDATA "pureandapplied" "newAeScript.dat" 
+    if (Test-Path $prefFile) {
+        $prefs = import-clixml  $prefFile
+    }
     return $prefs
 }
 
-function getDDir(){
+function getDDir() {
     Add-Type -AssemblyName System.Windows.Forms
     $browser = New-Object System.Windows.Forms.FolderBrowserDialog
+    $browser.description = "Select the Scripts folder for new AE scripts"
     $null = $browser.ShowDialog()
     return $browser.SelectedPath
 }
